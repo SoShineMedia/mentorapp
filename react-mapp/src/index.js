eabefd519ebe1e7c59e9a1579240a21d9c5ecb08
K-White <kenroy@nuco.io>
2020-02-05 15:50:25 -0400

    latest react updates

--- react-mapp/src/index.js
+++ react-mapp/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import { createBrowserHistory } from "history";
+//import { Router, Route, Switch, Redirect } from "react-router-dom";
+//import { Router, Route, IndexRedirect, hashHistory} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './css/index.css';
+import App from './js/App';
+//import Home from './js/components/view/App';
+//import Layout from './js/components/layout/Default';
+//import * as serviceWorker from './js/serviceWorker';
+
+//const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();

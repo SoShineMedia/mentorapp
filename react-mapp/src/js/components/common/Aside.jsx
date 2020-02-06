16938ed5c0b2a18179293912bed1fba5cc4906a1
K-White <kenroy@nuco.io>
2020-02-05 22:07:27 -0400

    latest react updates

--- react-mapp/src/js/components/common/Aside.jsx
+++ react-mapp/src/js/components/common/Aside.jsx
@@ -7,8 +7,8 @@ class Aside extends Component {
   render() {
       return (
           
-                <aside class="control-sidebar control-sidebar-dark">                    
-                    <div class="p-3">
+                <aside className="control-sidebar control-sidebar-dark">                    
+                    <div className="p-3">
                     <h5>Title</h5>
                     <p>Sidebar content</p>
                     </div>

13633145e1e110bbd132f51a370d4cd989c57d85
KenroyWhite <whitekenroy@yahoo.com>
2020-06-22 16:36:21 -0400

    update dashboard

--- src/js/components/view/Home.jsx
+++ src/js/components/view/Home.jsx
@@ -28,15 +28,15 @@ class App extends Component {
       <div className="container-fluid">
          { /* Info boxes  */ }
         <div className="row">
-          <div className="col-12 col-sm-6 col-md-3">
+          <div className="col-12 col-sm-6 col-md-4">
             <div className="info-box">
               <span className="info-box-icon bg-info elevation-1"><i className="fas fa-cog"></i></span>
 
               <div className="info-box-content">
-                <span className="info-box-text">PROJECTS</span>
+                <span className="info-box-text">QUESTIONS</span>
                 <span className="info-box-number">
                   10
-                  <small>%</small>
+                  
                 </span>
               </div>
                { /* /.info-box-content  */ }
@@ -44,7 +44,7 @@ class App extends Component {
              { /* /.info-box  */ }
           </div>
            { /* /.col  */ }
-          <div className="col-12 col-sm-6 col-md-3">
+          <div className="col-12 col-sm-6 col-md-4">
             <div className="info-box mb-3">
               <span className="info-box-icon bg-danger elevation-1"><i className="fas fa-thumbs-up"></i></span>
 
@@ -61,12 +61,12 @@ class App extends Component {
            { /* fix for small devices only  */ }
           <div className="clearfix hidden-md-up"></div>
 
-          <div className="col-12 col-sm-6 col-md-3">
+          <div className="col-12 col-sm-6 col-md-4">
             <div className="info-box mb-3">
               <span className="info-box-icon bg-success elevation-1"><i className="fas fa-shopping-cart"></i></span>
 
               <div className="info-box-content">
-                <span className="info-box-text">GOALS</span>
+                <span className="info-box-text">INDUSTRY</span>
                 <span className="info-box-number">760</span>
               </div>
                { /* /.info-box-content  */ }
@@ -74,19 +74,7 @@ class App extends Component {
              { /* /.info-box  */ }
           </div>
            { /* /.col  */ }
-          <div className="col-12 col-sm-6 col-md-3">
-            <div className="info-box mb-3">
-              <span className="info-box-icon bg-warning elevation-1"><i className="fas fa-users"></i></span>
-
-              <div className="info-box-content">
-                <span className="info-box-text">CONTACTS</span>
-                <span className="info-box-number">2,000</span>
-              </div>
-               { /* /.info-box-content  */ }
-            </div>
-             { /* /.info-box  */ }
-          </div>
-           { /* /.col  */ }
+          
         </div>
          { /* /.row  */ }
 
@@ -102,7 +90,7 @@ class App extends Component {
               
               <div className="col-md-12">
               <div className="time-header">
-                        <h3>Recent Activities</h3>
+                        <h3>Recent Questions</h3>
               </div>
               <div className="timeline">
                     <div className="time-label">
